1435ead390845a083476a8c0bd911ef2f7345907
Mark Vasilkov <mvasilkov@gmail.com>
2022-09-28 21:11:41 +0300

    Easing functions 2

--- easing_functions/app.ts
+++ easing_functions/app.ts
@@ -5,7 +5,14 @@
  */
 'use strict'
 
+import { CanvasHandle } from '../node_modules/natlib/canvas/CanvasHandle.js'
+import { textCapsule } from '../node_modules/natlib/canvas/text.js'
 import { smootherstep, smoothstep } from '../node_modules/natlib/interpolation.js'
+
+const enum Settings {
+    padding = 4,
+    resolution = 200,
+}
 
 const identity = (x: number) => x
 
@@ -25,3 +32,65 @@ function getPropertyValue(property: string): number {
         .getPropertyValue(property)
         .replace(/px$/, '')
 }
+
+const height = getPropertyValue('--canvas-height')
+const width = getPropertyValue('--canvas-width')
+const titleLeft = 0.5 * width
+const titleTop = 0.5 * (height + width)
+const size = width - Settings.padding - Settings.padding
+
+function paintEasingFunction({ title, fn }: EasingFunction) {
+    const ch = new CanvasHandle(null, width, height, 2, con => {
+        con.fillStyle = '#000'
+        con.fillRect(0, 0, width, height)
+
+        con.textAlign = 'center'
+        con.textBaseline = 'middle'
+        const path = textCapsule(con, titleLeft, titleTop, '300 16', title, 8, 12)
+
+        con.strokeStyle = '#8cff9b'
+        con.stroke(path)
+
+        con.fillStyle = '#8cff9b'
+        con.fillText(title, titleLeft, titleTop)
+
+        con.translate(0, width)
+        con.scale(1, -1)
+        con.translate(Settings.padding + 0.5, Settings.padding + 0.5)
+
+        con.beginPath()
+        con.moveTo(0, 0)
+        con.lineTo(0, size)
+        con.moveTo(0, 0)
+        con.lineTo(size, 0)
+
+        con.strokeStyle = '#ffe091'
+        con.stroke()
+
+        con.beginPath()
+        con.moveTo(6, 6)
+        con.lineTo(size, size)
+
+        con.setLineDash([9, 9])
+        con.stroke()
+        con.setLineDash([])
+
+        con.beginPath()
+        con.moveTo(0, 0)
+
+        for (let n = 0; n < Settings.resolution; ++n) {
+            const t = n / Settings.resolution
+            con.lineTo(t * size, fn(t) * size)
+        }
+
+        con.lineTo(size, size)
+
+        con.lineWidth = 2
+        con.strokeStyle = '#78fae6'
+        con.stroke()
+    })
+
+    document.getElementById('content')!.appendChild(ch.canvas)
+}
+
+easingFunctions.forEach(paintEasingFunction)
