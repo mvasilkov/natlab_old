c7568e2fda56095ef774bcd6b03c6ae61ce49928
Mark Vasilkov <mvasilkov@gmail.com>
2022-10-31 14:56:08 +0200

    Keyboard controls

--- dda/app.ts
+++ dda/app.ts
@@ -6,6 +6,7 @@
 'use strict'
 
 import { CanvasHandle } from '../node_modules/natlib/canvas/CanvasHandle.js'
+import { Input, Keyboard } from '../node_modules/natlib/controls/Keyboard.js'
 import { dda } from '../node_modules/natlib/dda.js'
 import { startMainloop } from '../node_modules/natlib/scheduling/mainloop.js'
 import { Vec2 } from '../node_modules/natlib/Vec2.js'
@@ -14,6 +15,7 @@ const enum Settings {
     height = 10,
     width = 10,
     tileSize = 32,
+    speed = 0.05,
 }
 
 const tiles = Array.from({ length: Settings.height },
@@ -26,7 +28,37 @@ const direction = new Vec2
 const canvas = new CanvasHandle(document.querySelector('#canvas'),
     Settings.width * Settings.tileSize, Settings.height * Settings.tileSize)
 
+const keyboard = new Keyboard
+keyboard.addEventListeners(document)
+
 function update() {
+    // Start point
+    if (keyboard.state[Input.LEFT_A]) {
+        startPoint.x -= Settings.speed
+    }
+    if (keyboard.state[Input.UP_W]) {
+        startPoint.y -= Settings.speed
+    }
+    if (keyboard.state[Input.RIGHT_D]) {
+        startPoint.x += Settings.speed
+    }
+    if (keyboard.state[Input.DOWN_S]) {
+        startPoint.y += Settings.speed
+    }
+
+    // End point
+    if (keyboard.state[Input.LEFT]) {
+        endPoint.x -= Settings.speed
+    }
+    if (keyboard.state[Input.UP]) {
+        endPoint.y -= Settings.speed
+    }
+    if (keyboard.state[Input.RIGHT]) {
+        endPoint.x += Settings.speed
+    }
+    if (keyboard.state[Input.DOWN]) {
+        endPoint.y += Settings.speed
+    }
 }
 
 function paint() {
