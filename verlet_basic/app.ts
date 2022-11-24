9b841861d9d1c5ac7bf6427ffa516fb420d7ed99
Mark Vasilkov <mvasilkov@gmail.com>
2022-11-24 21:15:46 +0200

    Update

--- verlet_basic/app.ts
+++ verlet_basic/app.ts
@@ -6,7 +6,7 @@
 'use strict'
 
 import { CanvasHandle } from '../node_modules/natlib/canvas/CanvasHandle.js'
-import { Keyboard } from '../node_modules/natlib/controls/Keyboard.js'
+import { Input, Keyboard } from '../node_modules/natlib/controls/Keyboard.js'
 import { startMainloop } from '../node_modules/natlib/scheduling/mainloop.js'
 import { Polygon } from '../node_modules/natlib/verlet/objects/Polygon.js'
 import { Scene } from '../node_modules/natlib/verlet/Scene.js'
@@ -15,6 +15,11 @@ import { getPropertyValue } from '../shared/shared.js'
 
 const enum Settings {
     backgroundColor = '#000',
+    gravity = 0.9,
+    viscosity = 0.99,
+    stiffness = 0.2,
+    mass = 1,
+    groundFriction = 0.9,
 }
 
 const PaintPolygon = WithPaintMethod(Polygon)
@@ -29,7 +34,8 @@ const objects = Array.from({ length: 4 }, (_, n) => {
     const x = 0.2 * width * (n + 1)
     const y = 0.5 * height
     const rotate = Math.PI * (1 / N + 1)
-    return new PaintPolygon(scene, N, x, y, 48, rotate)
+    return new PaintPolygon(scene, N, x, y, 48, rotate, 0, Settings.viscosity,
+        Settings.stiffness, Settings.mass, Settings.groundFriction)
 })
 
 const canvas = new CanvasHandle(document.querySelector('#canvas'), width, height)
@@ -37,7 +43,15 @@ const canvas = new CanvasHandle(document.querySelector('#canvas'), width, height
 const keyboard = new Keyboard
 keyboard.addEventListeners(document)
 
+let spacePressed = keyboard.state[Input.SPACE]
+
 function update() {
+    if (keyboard.state[Input.SPACE] && !spacePressed) {
+        const gravity = scene.vertices[0]!.gravity === 0 ? Settings.gravity : 0
+        scene.vertices.forEach(v => v.gravity = gravity)
+    }
+    spacePressed = keyboard.state[Input.SPACE]
+
     scene.update()
 }
 
