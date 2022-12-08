2512125f7bc9032028bf175bece073653362b167
Mark Vasilkov <mvasilkov@gmail.com>
2022-12-08 22:38:21 +0200

    Node.js wrapper for the Python perlin function, written with the help of both Copilot and ChatGPT

--- test/PerlinNoise/app.js
+++ test/PerlinNoise/app.js
@@ -5,7 +5,30 @@
  */
 'use strict'
 
+import { spawn } from 'node:child_process'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { PerlinNoise } from 'natlib/noise/PerlinNoise.js'
 import p from './permutation.json' assert { type: 'json' }
 
+const modulePath = fileURLToPath(import.meta.url)
+const __dirname = dirname(modulePath)
+
+function pythonPerlin(x, y, z) {
+    const p = spawn('python', [join(__dirname, 'rosettacode.py'), x, y, z])
+    return new Promise((resolve, reject) => {
+        p.stdout.on('data', data => {
+            resolve(parseFloat(data.toString('utf8')))
+        })
+        p.stderr.on('data', data => {
+            reject(data.toString('utf8'))
+        })
+    })
+}
+
 const perlin = new PerlinNoise(p.concat(p))
+
+async function run() {
+}
+
+run()
